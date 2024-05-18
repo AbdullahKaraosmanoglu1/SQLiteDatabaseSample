060a443cecbe790d82f831741b12c0c7b673e3d5
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-05-18 13:05:16 +0300

    Add project files.

---
+++ SQLiteSample/Controllers/RpgCharacterController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLiteSample.Data;
+using SQLiteSample.Entities;
+
+namespace SQLiteSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RpgCharacterController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public RpgCharacterController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<RpgCharacter>>> CreateCharacter(RpgCharacter character)
+        {
+            await _dataContext.AddAsync(character);
+
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.RpgCharacters.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<RpgCharacter>> GetAllCharacter()
+        {
+            return Ok(await _dataContext.RpgCharacters.ToListAsync());
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RpgCharacter>> GetCharacter(int id)
+        {
+            var character = await _dataContext.RpgCharacters.FindAsync(id);
+            if (character == null)
+            {
+                return BadRequest("Character Not Found");
+            }
+            return Ok(character);
+        }
+    }
+}
---
+++ SQLiteSample/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLiteSample.Entities;
+
+namespace SQLiteSample.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<RpgCharacter> RpgCharacters => Set<RpgCharacter>();
+    }
+}
